5958b57613dbc48e19fc3e362df6dc4a688641f6
MaksMN <maksimovmn78@gmail.com>
2024-06-15 18:25:51 +0300

    implement simple file class

--- App/SimpleFile/SimpleFile.php
+++ App/SimpleFile/SimpleFile.php
@@ -4,5 +4,14 @@ class SimpleFile extends File
 {
     public function __construct(string $file_path, $perms = 0600)
     {
+        $fd = fopen($file_path, 'c');
+        if ($fd === false) {
+            $error = error_get_last();
+            throw new Exception("ERROR: SimpleFile::__construct(): Could not open a file. " . $error['message'], 1);
+        }
+        fclose($fd);
+        chmod($file_path, $perms);
+        $this->fpath = $file_path;
+        $this->perms = $perms;
     }
 }
