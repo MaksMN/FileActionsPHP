bfbb6388b5162848bb0604ff0ef788d4a1c1432f
MaksMN <maksimovmn78@gmail.com>
2024-06-15 17:32:18 +0300

    inheritance classes

---
+++ App/RandomFile/RandomFile.php
@@ -0,0 +1,80 @@
+<?php
+
+/**
+ * Создает объект обработки уникального файла с рандомным именем.
+ */
+class RandomFile extends File
+{
+    /**
+     * @param dir string    Directory for file
+     * @param ext string|null    File extension
+     * @param time_prefix_format string|null    Prefix format - date_time. NULL - ignore
+     * @param prefix string Custom prefix
+     * @param random_len int Random string length
+     * @param lock_file_name string Lock file name
+     */
+    public function __construct(
+        string $dir,
+        string $ext = null,
+        $perms = 0600,
+        string $time_prefix_format = 'd_m_Y__H_i_s__',
+        string $prefix = '',
+        int $random_len = 10,
+        string $lock_file_name = 'lock'
+    ) {
+        if (file_exists($dir)) {
+            if (!is_dir($dir)) {
+                $error = error_get_last();
+                throw new Exception("ERROR: RandomFile::__construct(): directory is file " . $error['message'], 1);
+            }
+        } else {
+            mkdir($dir);
+        }
+
+        $lock_file = fopen($dir . '/' . $lock_file_name, 'c');
+        if (!$lock_file) {
+            $error = error_get_last();
+            throw new Exception("ERROR: RandomFile::__construct(): Could not get a file lock. " . $error['message'], 1);
+        }
+        if (!flock($lock_file, LOCK_EX)) {
+            $error = error_get_last();
+            throw new Exception("ERROR: RandomFile::__construct(): Could not get a file lock. " . $error['message'], 1);
+        }
+        $ext = is_null($ext) ? "" : ".$ext";
+        $file_path = $dir . "/" . $this->stampToDate($time_prefix_format) . $prefix . $this->randomString($random_len) . $ext;
+        while (file_exists($file_path)) {
+            $file_path = $dir . "/" . $this->stampToDate($time_prefix_format) . $prefix . $this->randomString($random_len) . $ext;
+        }
+        $file = fopen($file_path, 'c');
+        if (!$file) {
+            $error = error_get_last();
+            throw new Exception("ERROR: RandomFile::__construct(): Failed to create file $file_path. " . $error['message'], 1);
+        }
+        fclose($file);
+        fclose($lock_file);
+        chmod($file_path, $perms);
+        $this->fpath = $file_path;
+    }
+
+    private function randomString(int $length = 1): string
+    {
+        if ($length < 1)
+            $length = 1;
+        $bytes = openssl_random_pseudo_bytes($length);
+        $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+        $characters_len = strlen($characters);
+
+        $str_out = "";
+        foreach (str_split($bytes) as $ch) {
+            $num = ord($ch) % $characters_len;
+            $str_out .= $characters[$num];
+        }
+        return $str_out;
+    }
+    private function stampToDate(string $format = NULL): string
+    {
+        if (is_null($format) || strlen($format) == 0)
+            return "";
+        return date($format, time());
+    }
+}
---
+++ App/SimpleFile/SimpleFile.php
@@ -0,0 +1,8 @@
+<?php
+
+class SimpleFile extends File
+{
+    public function __construct(string $file_path, $perms = 0600)
+    {
+    }
+}
