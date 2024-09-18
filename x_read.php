09f38cc9c62567e51a7ca888bd7fd02af8df4434
MaksMN <maksimovmn78@gmail.com>
2024-09-18 12:14:40 +0300

    dev_002

---
+++ x_read.php
@@ -0,0 +1,15 @@
+<?php
+define('DEBUG_MODE', 1);
+define('APP_ROOT_DIR', __DIR__);
+define('DIRECT_ACCESS_KEY', 1);
+
+$file_path = __DIR__ . '/test/test.txt';
+require_once __DIR__ . '/App/autoload.php';
+try {
+    $simple_file = new File\SimpleFile($file_path, "c+", 0644);
+} catch (\Throwable $th) {
+    echo $th->getMessage();
+}
+
+echo "x_read start\n";
+echo "x_read: " . $simple_file->readLock();
